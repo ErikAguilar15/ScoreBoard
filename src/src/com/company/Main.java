a0e5f382ae144345a9a59c3778a6532526851a1d
alsendor <al_nucum@yahoo.com>
2018-11-05 23:17:12 -0800

    Added java file for connecting to database

---
+++ src/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.sql.*;
+
+public class Main {
+
+    static Connection conn = null;
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static Connection connect() {
+        if (conn != null)
+            return conn;
+        try{
+            conn = DriverManager.getConnection("jdbc:sqlite:/CSE111/ScoreBoard/src/statistics.db");
+            System.out.println("Establishing connection ");
+        }
+        catch(SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return conn;
+    }
+
+    public static void disconnect() {
+        if (conn!= null)
+            try {
+                conn.close();
+                System.out.println("Disconnection ");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+    }
+}
