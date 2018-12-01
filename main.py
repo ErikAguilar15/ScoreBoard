4a6e04003b0a838f99c763ade02b8264ee877fd6
alsendor <al_nucum@yahoo.com>
2018-11-30 21:35:20 -0800

    Updated with cursor

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import sqlite3
 from sqlite3 import Error
 
 def createConnection(db_file):
-    conn = sqlite3.connect(db_file);
+    return sqlite3.connect(db_file);
 
 def main():
-    createConnection("src/statistics.db")
+    conn = createConnection("src/statistics.db")
+    cursor = conn.cursor
