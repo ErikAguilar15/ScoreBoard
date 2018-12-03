6b0a15cb72632933ca5f2a137c4d33e451d3e4c3
alsendor <al_nucum@yahoo.com>
2018-12-03 02:23:06 -0800

    Execute query function

--- main.py
+++ main.py
@@ -6,9 +6,24 @@ from sqlite3 import Error
 def createConnection(db_file):
     return sqlite3.connect(db_file);
 
+def closeConnection(conn)
+    conn.close()
+
+def executeQuery(conn, query)
+    cursor.execute(query)
+    conn.commit()
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+
 def main():
     print ("Establishing connection.")
     conn = createConnection("src/statistics.db")
-    print ("Establishing cursor")
+    print ("Establishing cursor.")
     cursor = conn.cursor
-    db.close()
+    closeConnection()
+
+main()
