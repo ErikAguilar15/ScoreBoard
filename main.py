486d5a860a6715a2df135630a7084303cd1d545c
alsendor <al_nucum@yahoo.com>
2018-12-04 20:40:25 -0800

    REFIX

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ def viewHoopPlayerStats(conn):
 
 def viewHoopPlayerStatsByTeam(conn, team):
     c = conn.cursor()
-    sql = "SELECT * FROM hoopers WHERE team = \'%"
+    sql = "SELECT * FROM hoopers WHERE team LIKE \'%"
     sql += team
     sql += "%\'"
     c.execute(sql)
